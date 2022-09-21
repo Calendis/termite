479f8ca83cf6e5d05eb544db202d1cad3196c331
Calendis <calendis1@gmail.com>
2022-09-21 14:28:26 -0400

    tweaked example

--- examples/compositing.py
+++ examples/compositing.py
@@ -2,7 +2,7 @@ from termite import Frame, Compositor
 
 def main():
     inner_frame_1 = Frame.Positioned(True, 16, 9, 15, 10, 0, 0, 0, [])
-    inner_frame_2 = Frame.Positioned(True, 32, 10, 20, 14, 0, 0, 0, [])
+    inner_frame_2 = Frame.Positioned(True, 32, 10, 20, 15, 0, 0, 0, [])
     outer_frame = Frame.Full(0, 0, 0, [inner_frame_1, inner_frame_2])
     renders = outer_frame.render_all()
     render = Compositor.compose(*renders)
